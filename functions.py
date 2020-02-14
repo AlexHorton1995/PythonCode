5366a38614321c60ea3b786456f7074ef5230615
Alex Horton <alex.d.horton95@gmail.com>
2020-02-14 00:27:29 -0600

    added logic for list

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student.title()
+        students_titlecase = student["name"].title()
     return students_titlecase
 
 
@@ -14,18 +14,15 @@ def print_students_titlecase():
     print(students_titlecase)
 
 
-def add_student(name, student_id=332):
-    student = {"name": name, "student_id": student_id }
+def add_student(name, student_id):
+    student = {"name": name, "student_id": student_id}
     students.append(student)
-
-
-def var_args(name, **kwargs):
-    print(name)
-    print(kwargs["description"], kwargs["feedback"])
-
 
 student_list = get_students_titlecase()
 
-add_student(name="Mark", student_id=15)
+student_name = input("Enter Student Name: ")
+student_id = int(input("And their ID: "))
 
-var_args("Mark", description="Loves Python", feedback=None, pluralsight_subscriber=True)
+
+add_student(student_name, student_id)
+print_students_titlecase()
